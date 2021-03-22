742333eb4f199ae4298a0f77daf97a3ad4d76991
AndreiOsypchuk <andrewostin13@gmail.com>
2021-03-22 18:08:51 +0200

    removed dist folder from repo

--- server/dist/grqlconfig/todoListSchema.grql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.todoListQLSchema = void 0;
-var todoListQLSchema = "\n  type Todo {\n    _id: ID,\n    title: String!,\n    description: String,\n    done: Boolean\n  }\n  type TodoList {\n    _id: ID,\n    owner: ID!,\n    todos: [Todo!]\n  }\n";
-exports.todoListQLSchema = todoListQLSchema;
--- server/dist/grqlconfig/userSchema.grql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.userQLSchema = void 0;
-var userQLSchema = "\n  type User {\n    _id: ID,\n    name: String,\n    todoList: TodoList\n  }\n";
-exports.userQLSchema = userQLSchema;
